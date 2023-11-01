081060ad0b36addf1799e0dcd4c2c065c1203f22
Chinmaya Pradhan <118071349+chinmaya10000@users.noreply.github.com>
2023-11-01 12:00:46 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,16 +1,1 @@
-def buildImage() {
-    echo "building the docker image.."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t chinmayapradhan/reddit-app:1.0 .'
-        sh "echo $PASS | docker login -u $USER --passowrd-stdin"
-        sh 'docker push chinmayapradhan/reddit-app:1.0'
-    }
-}
-def deployApp() {
-    echo "Deploy docker image to EC2.."
 
-    def dockerComposeCmd = "docker-compose up -d"
-    sshagent(['ec2-server-key']) {
-        sh "ssh -o StrictHostKeyChecking=no ec2-user@3.19.141.157 '${dockerComposeCmd}'"
-    }
-}
